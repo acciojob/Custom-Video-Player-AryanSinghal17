250fbf898cf3b46fdf371753d17df9f2760b12b6
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-13 00:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,51 +4,49 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider'); 
+const ranges = player.querySelectorAll('.player__slider');
 
-//volume
-ranges.forEach((range) =>{
-  range.addEventListener("input",function(){
-
-     if(range.name === "volume") {
-            video.volume = range.value;
-      }
-  });
-});
-// speed
-ranges.forEach((range) =>{
-  range.addEventListener("input" ,function(){
-    if(range.name === "playbackRate"){
-      video.playbackRate = range.value;
+// Play / Pause
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
     }
-  });
-});
-// for button play and pause
-let togglebtn = true;
-toggle.addEventListener("click", function() {
+}
 
-    if(togglebtn){
-      video.play();
-      togglebtn = false;
-      toggle.innerText = "❚❚";
-    }else{
-      video.pause();
-      togglebtn = true;
-      toggle.innerText = "►";
-    }
+toggle.addEventListener('click', togglePlay);
+
+// Update button icon
+function updateButton() {
+    toggle.textContent = video.paused ? '►' : '❚❚';
+}
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+// Volume + Playback Speed
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+ranges.forEach(range => {
+    range.addEventListener('input', handleRangeUpdate);
 });
 
+// Skip buttons
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+skipButtons.forEach(button => {
+    button.addEventListener('click', skip);
+});
+
+// Progress Bar
 function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100;
     progressBar.style.flexBasis = `${percent}%`;
 }
 
-video.addEventListener("timeupdate", handleProgress);
-
-// skip button
-
-skipButtons.forEach((button) => {
-  button.addEventListener("click",function(){
-    video.currentTime += Number(button.dataset.skip);
-  });
-});
+video.addEventListener('timeupdate', handleProgress);
